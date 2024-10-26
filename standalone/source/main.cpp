7441976ce162446e682776b874b43e1d9fe08e13
ananthvk <123456789987654321sr@gmail.com>
2024-10-27 01:28:39 +0530

    Removed cxxopts for now to improve compile time

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -3,35 +3,15 @@
 #include <pinedb/storage.h>
 #include <pinedb/version.h>
 
-#include <cxxopts.hpp>
 #include <iostream>
 #include <string>
 #include <unordered_map>
 
 auto main(int argc, char** argv) -> int {
-  cxxopts::Options options(*argv, "PineDB server program");
-
-  // clang-format off
-  options.add_options()
-    ("h,help", "Show help")
-    ("v,version", "Print the current version number")
-  ;
-  // clang-format on
-
-  auto result = options.parse(argc, argv);
-
-  if (result["help"].as<bool>()) {
-    std::cout << options.help() << std::endl;
-    return 0;
-  }
-
-  if (result["version"].as<bool>()) {
-    std::cout << "PineDB, version " << PINEDB_VERSION << std::endl;
-    return 0;
-  }
-
   pinedb::DiskStorageBackend b("db_file_1", pinedb::config::PAGE_SIZE);
-  b.create_new_page();
+  // b.create_new_page();
+  uint8_t buf[pinedb::config::PAGE_SIZE];
+  b.read_page(8, buf);
 
   return 0;
 }
